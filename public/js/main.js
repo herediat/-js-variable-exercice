9803cf2ee95f53169085709d4071d077ac485b6a
herediat <ts.heredia@outlook.com>
2021-10-10 04:59:57 +0200

    exos done

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+// # Exercice 1 
+// - Déclarez une variable qui reçoit 1 chiffre
+// - Déclarez une variable qui variable reçoit 1 autre chiffre
+// - Afficher l'addition, soustraction, division et multiplication entre les 2 chiffres
+//     - exemple: ` variable1 + variable2 = resultat `
+
+// let monNombre = 50;
+// let monNombre2 = 60;
+// console.log(`${monNombre} + ${monNombre2} = ${monNombre + monNombre2}`);
+// console.log(`${monNombre} - ${monNombre2} = ${monNombre - monNombre2}`);
+// console.log(`${monNombre} / ${monNombre2} = ${monNombre / monNombre2}`);
+// console.log(`${monNombre} * ${monNombre2} = ${monNombre * monNombre2}`);
+//#######################################################################
+
+// # Exercice 2 
+// - Demander via un prompt l'age de l'utilisateur
+// - Demander via un prompt le prénom de l'utilisateur'
+// - Afficher dans la console le message : "prenom a age ans cette année"
+// - Afficher dans la console le message : "prenom aura age ans dans 3 ans" (attention avec l'age de la personne)
+
+// let age = Number(prompt('ton âge ?'));
+// let numberConvert = parseInt(age)
+// console.log(typeof age);
+// let prenom = prompt('ton prenom ?');
+
+// console.log(`${prenom} a ${age} ans cette année`);
+// console.log(`${prenom} a ${age+3} ans dans 3ans`);
+//########################################################################
+
+// # Exercice 3 
+// - Créez une variable avec l'année de votre naissance
+// - Créez une variable avec l'année actuelle
+// - Affichez votre age à l'année actuelle
+// - Affichez votre age 2 ans aprés l'année actuelle
+
+// let annee = 1998;
+// let actuelle = 2021;
+// console.log(`ton age actuelle : ${actuelle - annee} ans`);
+// console.log(`ton age dans deux ans : ${(actuelle - annee) + 2} ans`);
+
